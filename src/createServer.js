6be1da8e8ec3736fa669f6da4915bfe826ac1457
Anton Kryzhanivskyi <firechant2@gmail.com>
2025-10-08 10:22:35 +0300

    bug-fix

--- src/createServer.js
+++ src/createServer.js
@@ -37,7 +37,7 @@ function createServer() {
     }
 
     if (errors.length > 0) {
-      res.writeHead(400, 'Bad request', { 'Content-Type': 'text/plain' });
+      res.writeHead(400, 'Bad request', { 'Content-Type': 'application/json' });
       res.end(JSON.stringify({ errors }));
 
       return;
